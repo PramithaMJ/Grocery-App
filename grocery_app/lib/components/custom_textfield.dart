fcbf8cbf41c3e87bb5d174b155ab56d10565e411
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-12-08 06:56:00 +0530

    V1.1

---
+++ grocery_app/lib/components/custom_textfield.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/utils/constants/app_colors.dart';
+
+class CustomerTextField extends StatelessWidget {
+  const CustomerTextField({
+    super.key,
+    required this.hintText,
+    required this.labelText,
+    this.isObscure = false,
+  });
+
+  final String hintText;
+  final String labelText;
+  final bool isObscure;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: AppColors.ash.withOpacity(.4),
+          offset: const Offset(0, 2),
+          blurRadius: 10,
+        )
+      ]),
+      child: TextField(
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(color: AppColors.ash),
+          label: Text(
+            labelText,
+            style: const TextStyle(
+              color: AppColors.ash,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(color: AppColors.primaryColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- grocery_app/lib/screens/auth/signup.dart
+++ grocery_app/lib/screens/auth/signup.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:grocery_app/components/app_logo.dart';
+import 'package:grocery_app/components/custom_textfield.dart';
 import 'package:grocery_app/utils/constants/app_colors.dart';
 
 class Signup extends StatefulWidget {
@@ -15,120 +16,78 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Sign Up Page",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: AppColors.primaryColor,
-                  fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Sign Up Page",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: AppColors.primaryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 41,
-              ),
-              AppLogo(),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: AppColors.ash.withOpacity(.4),
-                    offset: Offset(0, 2),
-                    blurRadius: 10,
-                  )
-                ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter your Email or Username here",
-                    hintStyle: TextStyle(color: AppColors.ash),
-                    label: Text(
-                      "Email",
-                      style: TextStyle(
-                        color: AppColors.ash,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: AppColors.primaryColor),
+                const SizedBox(
+                  height: 41,
+                ),
+                const AppLogo(),
+                const SizedBox(
+                  height: 30,
+                ),
+                const CustomerTextField(
+                  hintText: "Enter your UserName",
+                  labelText: "Username",
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const CustomerTextField(
+                  hintText: "Enter your Email",
+                  labelText: "Email",
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const CustomerTextField(
+                  hintText: "Enter your Password",
+                  labelText: "Password",
+                  isObscure: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Already have an account?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: AppColors.ash.withOpacity(.4),
-                    offset: Offset(0, 2),
-                    blurRadius: 10,
-                  )
-                ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter your Email or Username here",
-                    hintStyle: TextStyle(color: AppColors.ash),
-                    label: Text(
-                      "Email",
-                      style: TextStyle(
-                        color: AppColors.ash,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: AppColors.primaryColor),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 250,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
                     ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                  BoxShadow(
-                    color: AppColors.ash.withOpacity(.4),
-                    offset: Offset(0, 2),
-                    blurRadius: 10,
-                  )
-                ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter your Email or Username here",
-                    hintStyle: TextStyle(color: AppColors.ash),
-                    label: Text(
-                      "Email",
-                      style: TextStyle(
-                        color: AppColors.ash,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: AppColors.primaryColor),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
